c6f64b7adb917b9f1bc7c0d66092374997bf89c7
Smoliak Yaroslav <smoliak.yaroslav@gmail.com>
2020-01-28 16:30:38 +0200

    - exception developer page

--- src/Untappd.KolosBot/Untappd.KolosBot/Startup.cs
+++ src/Untappd.KolosBot/Untappd.KolosBot/Startup.cs
@@ -27,10 +27,7 @@ namespace Untappd.KolosBot
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
 
