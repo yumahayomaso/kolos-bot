c2068d4af506e34a6e50ae3476c1f9cf92a176f2
Smoliak Yaroslav <smoliak.yaroslav@gmail.com>
2020-01-28 16:19:09 +0200

    - move to webhook

---
+++ src/Untappd.KolosBot/Untappd.KolosBot/Controllers/TelegramController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Untappd.KolosBot.Infrastucture;
+
+namespace Untappd.KolosBot.Controllers
+{
+    [ApiController]
+    public class TelegramController : ControllerBase
+    {
+        private readonly TelegramBotClient _botClient;
+
+        public TelegramController(TelegramBotClient botClient)
+        {
+            _botClient = botClient;
+        }
+
+        [HttpPost("webhook")]
+        public async Task<IActionResult> MessageUpdate(Update update)
+        {
+            var message = update.Message;
+            try
+            {
+                if (message.Text == "/pio")
+                {
+                    var parser = new UntappdParser();
+                    var beers = await parser.GetBeers();
+
+                    foreach (var beer in beers)
+                    {
+                        await _botClient.SendPhotoAsync(message.Chat.Id, new InputOnlineFile(new Uri(beer.ImageUrl)));
+                        await _botClient.SendTextMessageAsync(
+                            chatId: message.Chat,
+                            text: beer.ToString(),
+                            ParseMode.Markdown
+                        );
+                    }
+                }
+
+
+                if (update.Type == UpdateType.Message && message.Type == MessageType.Text && message.Text.Contains("/pidor"))
+                {
+                    await Task.Delay(5000);
+                    await _botClient.SendTextMessageAsync(
+                        chatId: message.Chat,
+                        text: "Оййй та бля пацани"
+                    );
+                    await _botClient.SendTextMessageAsync(
+                        chatId: message.Chat,
+                        text: "І так панятно що"
+                    );
+                    await _botClient.SendTextMessageAsync(
+                        chatId: message.Chat,
+                        text: "ЙОНЗА ПЕДРИЛО"
+                    );
+                }
+
+                if (message.Text != null && yosaList.Any(x => message.Text.Contains(x, StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    var rand = new Random();
+                    var randIndex = rand.Next(0, yosaJokeList.Count - 1);
+                    await _botClient.SendTextMessageAsync(
+                        chatId: message.Chat,
+                        text: yosaJokeList[randIndex]
+                    );
+                }
+            }
+            catch (Exception exception)
+            {
+                // ignored
+            }
+
+            return Ok();
+        }
+
+        private static List<string> yosaList = new List<string>()
+        {
+            "Йоса",
+            "Йонза",
+            "Йондза",
+            "Йондзолик",
+            "Йонзолик",
+            "Йосиф",
+            "Йосип",
+            "Йося",
+            "Йосу",
+            "Йосю",
+            "Йонз",
+            "Йондз",
+            "Йос",
+        };
+
+        private static List<string> yosaJokeList = new List<string>
+        {
+            "Йоса педик",
+            "Йоса як там тренер?",
+            "Педрило шо ти?",
+            "Мене взаламали, вірус, Йоса Педик",
+            "Йоса петук",
+            "Педрило як твоє очко?",
+            "ЙОООСА ПЕДДДИК",
+            "Шо там по педрилам, Йоса?",
+            "Йоса, та ладно ми і так знаємо шо ти педрило",
+            "За попередніми даними Йондза педик",
+            "АЛО, ПРІЙОМ, ПЕДРИЛО Йос",
+            "Була би срака, а Йоса появиться",
+            "ТСН(НОВИНИ): В Червонограді згвалтували хлопчика в очко всі думають на Йодзолика",
+            "HOW DARE YOU?>>>yosyf pedrilo",
+        };
+    }
+}
--- src/Untappd.KolosBot/Untappd.KolosBot/Program.cs
+++ src/Untappd.KolosBot/Untappd.KolosBot/Program.cs
@@ -1,50 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
-using System.Timers;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Telegram.Bot;
-using Telegram.Bot.Args;
-using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InputFiles;
-using Untappd.KolosBot.Infrastucture;
 
 namespace Untappd.KolosBot
 {
     public class Program
     {
         private static ITelegramBotClient _botClient;
-        private static KolosDbContext _dbContext;
-        private static Timer _timer;
-
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             try
             {
                 var host = CreateHostBuilder(args).Build();
                 var token =  Environment.GetEnvironmentVariable("BOT_TOKEN");
                 _botClient = new TelegramBotClient(token);
-                _botClient.OnMessage += Bot_OnMessage;
-                _botClient.StartReceiving();
-
-                _timer = new Timer();
-                _timer.Interval = TimeSpan.FromMinutes(28).TotalMilliseconds;
-                _timer.AutoReset = true;
-                _timer.Elapsed += delegate(object sender, ElapsedEventArgs eventArgs)
+                //TODO move to startup
+                var webhook = await _botClient.GetWebhookInfoAsync();
+                if (string.IsNullOrWhiteSpace(webhook.Url))
                 {
-                    var ukrHour = DateTime.UtcNow.Hour;
-                    if (ukrHour < 7 && ukrHour > 0)
-                        return;
-                    
-                    var httpClient = new HttpClient();
-                    httpClient.GetAsync("https://zkolos-bot.herokuapp.com/").GetAwaiter().GetResult();
-                };
-                
-                _timer.Start();
+                    await _botClient.SetWebhookAsync("https://zkolos-bot.herokuapp.com/webhook");
+                }
+
                 host.Run();
             }
             catch (Exception e)
@@ -64,98 +44,14 @@ namespace Untappd.KolosBot
             return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
+                    webBuilder.UseStartup<Startup>().ConfigureServices(services =>
+                            {
+                                services.AddSingleton(_botClient);
+                            })
                     .UseUrls("http://*:" + port);
                 });
         }
 
-        static async void Bot_OnMessage(object sender, MessageEventArgs e)
-        {
-            try
-            {
-                if (e.Message.Text == "/pio")
-                {
-                    var parser = new UntappdParser();
-                    var beers = await parser.GetBeers();
 
-                    foreach (var beer in beers)
-                    {
-                        await _botClient.SendPhotoAsync(e.Message.Chat.Id, new InputOnlineFile(new Uri(beer.ImageUrl)));
-                        await _botClient.SendTextMessageAsync(
-                            chatId: e.Message.Chat,
-                            text: beer.ToString(),
-                            ParseMode.Markdown
-                        );
-                    }
-                }
-
-
-                if (e.Message.Type == MessageType.Text && e.Message.Text.Contains("/pidor"))
-                {
-                    await Task.Delay(5000);
-                    await _botClient.SendTextMessageAsync(
-                        chatId: e.Message.Chat,
-                        text: "Оййй та бля пацани"
-                    );
-                    await _botClient.SendTextMessageAsync(
-                        chatId: e.Message.Chat,
-                        text: "І так панятно що"
-                    );
-                    await _botClient.SendTextMessageAsync(
-                        chatId: e.Message.Chat,
-                        text: "ЙОНЗА ПЕДРИЛО"
-                    );
-                }
-
-                if (e.Message.Text != null && yosaList.Any(x => e.Message.Text.Contains(x, StringComparison.InvariantCultureIgnoreCase)))
-                {
-                    var rand = new Random();
-                    var randIndex = rand.Next(0, yosaJokeList.Count - 1);
-                    await _botClient.SendTextMessageAsync(
-                        chatId: e.Message.Chat,
-                        text: yosaJokeList[randIndex]
-                    );
-                }
-            }
-            catch (Exception exception)
-            {
-                // ignored
-            }
-        }
-
-        private static List<string> yosaList = new List<string>()
-        {
-            "Йоса",
-            "Йонза",
-            "Йондза",
-            "Йондзолик",
-            "Йонзолик",
-            "Йосиф",
-            "Йосип",
-            "Йося",
-            "Йосу",
-            "Йосю",
-            "Йонз",
-            "Йондз",
-            "Йос",
-        };
-
-        private static List<string> yosaJokeList = new List<string>
-        {
-            "Йоса педик",
-            "Йоса як там тренер?",
-            "Педрило шо ти?",
-            "Мене взаламали, вірус, Йоса Педик",
-            "Йоса петук",
-            "Педрило як твоє очко?",
-            "ЙОООСА ПЕДДДИК",
-            "Шо там по педрилам, Йоса?",
-            "Йоса, та ладно ми і так знаємо шо ти педрило",
-            "За попередніми даними Йондза педик",
-            "АЛО, ПРІЙОМ, ПЕДРИЛО Йос",
-            "Була би срака, а Йоса появиться",
-            "ТСН(НОВИНИ): В Червонограді згвалтували хлопчика в очко всі думають на Йодзолика",
-            "HOW DARE YOU?>>>yosyf pedrilo",
-        };
     }
 }
